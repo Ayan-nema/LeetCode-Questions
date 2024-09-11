3752bcadcd2eec33b393d99353993db434b3184c
Ayan_Nema <ayanashish2002@gmail.com>
2024-09-11 08:12:16 +0530

    Daily ques-11/9/2024

--- 2220_Minimum Bit Flips to Convert Number.cpp
+++ 2220_Minimum Bit Flips to Convert Number.cpp
@@ -11,3 +11,33 @@ public:
         return count;
     }
 };
+
+//Approach 2 
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int num= start ^ goal;
+        int count =0;
+        while(num)
+        {
+            num= num & num-1;//agar kisi no. ko khud ke ek kam se zor karo to usko right most one bit--> zero bit ho jaata hai
+            count++;
+        }
+        return count;
+    }
+};
+
+//Approach 3
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int num= start ^ goal;
+        int count = 0;
+        while(num>0)
+        {
+            if(num & 1) count++;
+            num=num/2;
+        }
+        return count;
+    }
+};
